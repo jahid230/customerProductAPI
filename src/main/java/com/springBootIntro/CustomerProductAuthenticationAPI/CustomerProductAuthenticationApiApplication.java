681e29c4c91cd48aa18d5bcb52640d32586116c7
jahid230 <jahid.crypto@gmail.com>
2022-03-07 19:00:32 +0100

    Intial Project push with all necessary dependency

---
+++ src/main/java/com/springBootIntro/CustomerProductAuthenticationAPI/CustomerProductAuthenticationApiApplication.java
@@ -0,0 +1,13 @@
+package com.springBootIntro.CustomerProductAuthenticationAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerProductAuthenticationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerProductAuthenticationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springBootIntro/CustomerProductAuthenticationAPI/CustomerProductAuthenticationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBootIntro.CustomerProductAuthenticationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerProductAuthenticationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
